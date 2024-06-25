af687ead0f0592bd91496c7d10ec12e30a73a32b
monk007 <157161434+bdzb@users.noreply.github.com>
2024-06-25 09:05:54 +0530

    Update TopH.tsx

--- frontend/src/components/TopH.tsx
+++ frontend/src/components/TopH.tsx
@@ -1,42 +1,62 @@
-import React from 'react'
-import Navbar from './Navbar'
-import img from '../assets/car1.png'
-import './TopH.css'
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
+import img from '../assets/car1.png';
+import './TopH.css';
+
 const TopH = () => {
+  const [renderStep, setRenderStep] = useState(0);
+
+  useEffect(() => {
+    const timers = [
+      setTimeout(() => setRenderStep(1), 500), // Navbar
+      setTimeout(() => setRenderStep(2), 1000), // Left Content
+      setTimeout(() => setRenderStep(3), 1500), // Numbers
+      setTimeout(() => setRenderStep(4), 2000), // Right Content
+    ];
+
+    return () => timers.forEach(clearTimeout);
+  }, []);
+
   return (
-    <>
     <div id="tbody">
-      <div id="navTop"><Navbar/></div>
+      <div id="navTop" className={renderStep >= 1 ? 'slide-in-top' : ''}>
+        {renderStep >= 1 && <Navbar />}
+      </div>
+
       <div id="contentsTop">
-        <div id="leftContentTop" className='leftWidthTop'>
-          <p className='top'>Buy, sell & rent </p>
-          <p className='bottom'>reputable cars</p>
-          <p id='slogan'>Buy, sell & rent reputable cars . Renting is easy and fast with CarHub</p>
-        
-        <ul id='numbersTop' >
-          <div id='li-r-border' >
-          <li>
-          100+
-          </li>
-        
-          <li>
-            Car Brands
-          </li>
-          </div>
-          
-          <div className='widthTopNumbers'>
-          <li>100K+</li>
-          <li>Clients</li>
-          </div>
+        <div id="leftContentTop">
+        <div  className={`leftWidthTop ${renderStep >= 2 ? 'slide-in-bottom' : ''}`}>
+          {renderStep >= 2 && (
+            <>
+              <p className='top'>Buy, sell & rent</p>
+              <p className='bottom'>reputable cars</p>
+              <p id='slogan'>Buy, sell & rent reputable cars. Renting is easy and fast with CarHub</p>
+            </>
+          )}
+        </div>
+
+        <ul id='numbersTop' className={renderStep >= 3 ? 'slide-in-top' : ''}>
+          {renderStep >= 3 && (
+            <>
+              <div id='li-r-border'>
+                <li  className='bigNum'>100+</li>
+                <li>Car Brands</li>
+              </div>
+              <div className='widthTopNumbers'>
+                <li className='bigNum'>100K+</li>
+                <li>Clients</li>
+              </div>
+            </>
+          )}
         </ul>
         </div>
-        <div id="rightContentTop">
-          <img src={img} alt=""  id='carHome'/>
+        <div id="rightContentTop" className={renderStep >= 4 ? 'slide-in-right' : ''}>
+
+          {renderStep >= 4 && <img src={img} alt="Car" id='carHome' />}
         </div>
       </div>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default TopH
+export default TopH;
